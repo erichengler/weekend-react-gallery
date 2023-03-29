58285664bdb3674f6927a4a88444a3bf935a8c60
Erich Engler <erich.engler@hotmail.com>
2023-03-29 18:40:24 -0500

    added some imports and exports to components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
+import GalleryItem from '../GalleryItem/GalleryItem';
+import GalleryList from '../GalleryList/GalleryList';
 
 function App() {
+
+    // Axios GET Request to get data from /gallery
+
+
     return (
       <div className="App">
         <header className="App-header">
---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GalleryList () {
+
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default GalleryList;
