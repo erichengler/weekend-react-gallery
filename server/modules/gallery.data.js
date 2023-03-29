282707b2a6cc5d2673f8d100771e7fb9572f5e90
Erich Engler <erich.engler@hotmail.com>
2023-03-29 18:30:01 -0500

    created GalleryList and GalleryItem components

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, title: 'Goal', path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, title: 'Eagle', path: 'images/eagle.png', description: 'An eagle. He looks so soft. Are those feathers in your beak?', likes: 0 },
+    { id: 3, title: 'Hippo', path: 'images/hippo.png', description: 'A hippo floating just below the surface. Be afraid.', likes: 0 },
+    { id: 4, title: 'Panther', path: 'images/panther.png', description: 'This panther looks very friendly and polite.', likes: 0 }
 ];
 
 module.exports = galleryItems;
