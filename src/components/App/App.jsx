6a513f7b997d92a47e8b381988402e8470a01d3e
Erich Engler <erich.engler@hotmail.com>
2023-03-29 19:21:00 -0500

    wrote axios get request to get data from /gallery and useEffect

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import GalleryItem from '../GalleryItem/GalleryItem';
 import GalleryList from '../GalleryList/GalleryList';
 
 function App() {
+    const [galleryList, setGalleryList] = useState([])
 
     // Axios GET Request to get data from /gallery
+    const fetchGalleryList = () => {
+        axios.get('/gallery').then((response) => {
+            setGalleryList(response.data);
+        }).catch((error) => {
+            console.log(`Error in GET ${error}`);
+            alert('Something went wrong.');
+        })
+    };
+
+    useEffect(() => {
+        fetchGalleryList();
+    }, []);
 
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
+        <div className="App">
+            <header className="App-header">
+                <h1 className="App-title">Gallery of My Life</h1>
+            </header>
+            <p>Gallery goes here</p>
+            <img src="images/goat_small.jpg"/>
+        </div>
     );
 }
 
