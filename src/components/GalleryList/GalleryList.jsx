1a02b14167a8012fa020f5622ccaca8e8a6599cf
Erich Engler <erich.engler@hotmail.com>
2023-04-04 18:37:47 -0500

    more small fixes to container spacing

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import GalleryItem from '../GalleryItem/GalleryItem';
 import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 
 function GalleryList ({ galleryList, fetchGalleryList }) {
 
     return (
-        <Grid container spacing={0}>
-            {
-                galleryList.map((item) => (
-                    <GalleryItem 
-                        key={item.id}
-                        item={item}
-                        fetchGalleryList={fetchGalleryList}
-                    />
-                ))
-            }
-        </Grid>
+        <Container maxWidth='xl'>
+            <Grid container spacing={0} justifyContent='center'>
+                {
+                    galleryList.map((item) => (
+                        <GalleryItem 
+                            key={item.id}
+                            item={item}
+                            fetchGalleryList={fetchGalleryList}
+                        />
+                    ))
+                }
+            </Grid>
+        </Container>
 
     )
 }
